7604aff9200e733ae894132718864e1215316b39
Sahan Medhawa <dmsahanmedawa@gmail.com>
2026-05-01 15:22:36 +0530

    exercise 2

---
+++ my-app/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+    <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  );
+}
+
+export default NavBar;
